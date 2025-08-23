c064d7803568366dbf0f52e40256c560d2bee8c0
SauqiRizqullah <sauqi.risqullah@gmail.com>
2025-08-23 18:47:18 +0700

    [ADD] Repository

---
+++ src/main/java/com/personal/onlineclass/repository/TransactionDetailRepository.java
@@ -0,0 +1,9 @@
+package com.personal.onlineclass.repository;
+
+import com.personal.onlineclass.entity.TransactionDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionDetailRepository extends JpaRepository<TransactionDetail, String> {
+}
